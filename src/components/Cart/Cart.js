4170a63d28e261a3f0be98ef39c9f5c84f152f33
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-05-18 15:09:38 +0600

    stored cart data in local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
-            <p>selected items: {cart.length}</p> 
+            <p>selected items: {quantity}</p> 
+            <p>Total Price: ${total}</p>
+            <p>Total shipping: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
